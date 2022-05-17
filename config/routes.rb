9d99aea4fe74910b86a48be911dca12f53557c28
Robin Varetz <vrobin@student.42.fr>
2022-05-17 14:38:37 +0200

    finish exo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # new get
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  # new post
+  post 'tasks', to: 'tasks#create'
+  # edit get
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # edit post
+  patch 'tasks/:id', to: 'tasks#update'
+  # delete
+  delete 'tasks/:id', to: 'tasks#destroy'
+  # index
+  get 'tasks', to: 'tasks#index'
+  # show
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 end
